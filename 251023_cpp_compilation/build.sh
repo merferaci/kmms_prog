e5c4d8f5f08495649e9d52a2c00b71469e5d6cc5
merferaci <yatsina_pa@mail.ru>
2025-10-11 21:08:41 +0500

    Create cpp_compilation app

--- 251023_cpp_compilation/build.sh
+++
@@ -1,9 +0,0 @@
-MAIN=main.cpp
-APP=example
-
-if [ -f $APP ]; then rm $APP
-fi
-
-g++ $MAIN -o $APP
-
-./$APP
--- 251023_cpp_compilation/main.cpp
+++ 251023_cpp_compilation/main.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
-#include <string>
+#include "io.hpp"
+#include "sortings.hpp"
 
-int main(){
-	int k = 5;
-	std::cout << "Введите приветствие: ";
-	std::string str;
-	std::getline(std::cin, str);
-	std::cout << str << std::endl;
+int main() {
+    int arr[] = {1, 8, 2, 5, 3, 11};
+	const int arr_size = sizeof(arr) / sizeof(arr[0]);
+	
+	biv::print_array("Массив до сортировки:", arr, arr_size);
+    
+    biv::sortings::bubble_sort(arr, arr_size);
+
+    biv::print_array("Массив после сортировки:", arr, arr_size);
+
+    return 0;
 }
