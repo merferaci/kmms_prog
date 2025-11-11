5e7e308d4aba2a22e80ef44b7ff6a59db8db2b93
merferaci <yatsina_pa@mail.ru>
2025-11-11 22:52:15 +0300

    Update refactoring 2

--- 251029_refactoring_2/refactoring2.cpp
+++ 251029_refactoring_2/refactoring2.cpp
@@ -1,20 +1,54 @@
 #include <iostream>
 
-void merge(int arr[], int left, int mid, int right) {
-    int size = right - left + 1;
+void merge(int arr[], int left, int mid, int right);
+void merge_sort(int arr[], int size);
+void merge_sort_recursive(int arr[], int left, int right);
+
+int main() {
+    int size;
+    
+    std::cout << "Введите количество элементов: ";
+    std::cin >> size;
+    
+    int* arr = new int[size];
+    
+    std::cout << "Введите " << size << " элементов:" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cin >> arr[i];
+    }
+    
+    std::cout << std::endl << "Исходный массив: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    
+    merge_sort(arr, size);
+    
+    std::cout << "Отсортированный массив: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    
+    delete[] arr;
+    return 0;
+}
+
+void merge(int arr[], const int left, const int mid, const int right) {
+    const int size = right - left + 1;
     int* temp = new int[size];
         
     int i = left; 
     int j = mid + 1; 
     int k = 0;
     
-    while(i <= mid && j <= right){
-        if(arr[i] <= arr[j]){
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
             temp[k] = arr[i];
             i++;
             k++;
-        }
-        else{
+        } else {
             temp[k] = arr[j];
             j++;
             k++;
@@ -40,20 +74,7 @@ void merge(int arr[], int left, int mid, int right) {
     delete[] temp;
 }
 
-void merge_sort_recursive(int arr[], int left, int right) {
-    if (left >= right) {
-        return;
-    }
-
-    int mid = left + (right - left) / 2;
-
-    merge_sort_recursive(arr, left, mid);
-    merge_sort_recursive(arr, mid + 1, right);
-
-    merge(arr, left, mid, right);
-}
-
-void merge_sort(int arr[], int size) {
+void merge_sort(int arr[], const int size) {
     if (size == 0) {
         return;
     }
@@ -61,19 +82,15 @@ void merge_sort(int arr[], int size) {
     merge_sort_recursive(arr, 0, size - 1);
 }
 
-int main() {
-    int arr[] = {38, 27, 43, 3, 9, 82, 10, 1, 6, 20, 50, 7};
-    int size = sizeof(arr) / sizeof(arr[0]);
+void merge_sort_recursive(int arr[], const int left, const int right) {
+    if (left >= right) {
+        return;
+    }
 
-    for(int i = 0; i < size; ++i){
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;   
-    
-    merge_sort(arr, size);
-    for(int i = 0; i < size; ++i){
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
-    return 0;
+    const int mid = left + (right - left) / 2;
+
+    merge_sort_recursive(arr, left, mid);
+    merge_sort_recursive(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
 }
